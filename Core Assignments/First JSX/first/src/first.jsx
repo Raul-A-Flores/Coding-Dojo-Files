b056b80415e870b8450263c765d17e1c37871b8f
Raul Flores <raul.alexander.floress@gmail.com>
2022-05-04 23:35:55 -0500

    first commit

---
+++ Core Assignments/First JSX/first/src/first.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Component} from 'react';
+
+
+class First extends Component {    
+    render() {        
+        return (            
+           <div>
+            <h1>Hello Dojo!</h1>
+            <h2>Things I need to do:</h2>
+          
+              <ul>
+                  <li>Learn React</li>
+                  <li>Climb Mt. Everest</li>
+                  <li>Run a marathon</li>
+                  <li>Feed the dogs</li>
+                  <li>Take out the trash</li>
+
+
+              </ul>
+           </div>
+            
+        );    
+    }
+}
+
+export default First;
