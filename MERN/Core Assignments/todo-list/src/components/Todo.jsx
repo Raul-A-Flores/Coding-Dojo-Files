c41e49a2544405a33f7bb6f687db323627cde047
Raul Flores <raul.alexander.floress@gmail.com>
2022-05-11 08:39:31 -0500

    first commit

--- MERN/Core Assignments/todo-list/src/components/Todo.jsx
+++ MERN/Core Assignments/todo-list/src/components/Todo.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Todo = () =>{
 
+    const [todoList, setTodoList] = useState("")
+    const [todosList, setTodosList] = useState([]);
+
+    const handleToDo = (e) =>{
+        e.preventDefault();
+        setTodosList([...todosList, todoList])
+        
+
+
+    };
+
+    const handleDelete =(i) =>{
+
+        
+    }
     return(
         <div className='container'>
 
-            <form className='form-group col-6'>
+            <form className='form-group ' onSubmit ={(e) => {
+                handleToDo(e);
+            }}>
+
+
                 <h1> To Do List</h1>
-                <input className="form-control col-6" type="text" placeholder='Add todo'/>
+                <input onChange ={(e) =>{
+                    setTodoList(e.target.value)
+                }}
+
+                    className="form-control col-6" type="text" placeholder='Add todo'/>
                 
                 <button className="btn btn-primary mt-3" type='submit'> Add</button>
             </form>
             
-            <button className='btn btn-dark'>Delete</button>
-        </div>
+            
 
+
+            {
+            todosList.map((todoList,i) => {
+                return(
+                    <div key ={i}>
+                        <span>{todoList}</span>
+                        <button className='btn btn-dark'>Delete</button>
+                    </div>
+                );
+                })
+            }
+        </div>
+        
     )
 }
 
