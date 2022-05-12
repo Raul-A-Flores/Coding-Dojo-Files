88532a7b2f634ac14cd194a6c57352d7ba57803d
Raul Flores <raul.alexander.floress@gmail.com>
2022-05-12 11:24:53 -0500

    first commit

--- MERN/Practice Assignments/faker_api/server.js
+++ MERN/Practice Assignments/faker_api/server.js
@@ -1,8 +1,69 @@
-const express = requre("express");
-const faker = faker();
+const express = require("express");
+const { faker } = require('@faker-js/faker');
+
 const app = express();
 const port = 8000;
 
-app.get("/faker", (req,res) =>{
-    res.json({message: "Faker Api"});
+
+const createUser =() =>{
+    const newUser ={
+        first_name: faker.name.firstName(),
+        last_name: faker.name.lastName(),
+        phone: faker.phone.phoneNumber(),
+        password: faker.internet.password(),
+        _id: faker.datatype.uuid(),
+        
+        
+    };
+    return newUser;
+}
+
+
+const createCompany =() =>{
+    const newCompany ={
+        _id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        suffix: faker.company.companySuffix(),
+        address: address ={
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country()
+        }
+
+       
+        
+        
+    };
+    return newCompany;
+}
+let user1 = createUser();
+let company1 = createCompany();
+console.log(company1);
+console.log(user1)
+
+
+app.get("/new", (req,res) =>{
+    res.send({message: "Faker Api"});
 })
+app.get("/users/new", (req, res) => {
+    let newPerson = createUser();
+    console.log(newPerson);
+    res.json({ "result": newPerson });
+
+});
+
+app.get("/users/new/company", (req, res) => {
+    let newPerson = createUser();
+    let newCompany = createCompany();
+    console.log(newPerson, newCompany);
+    res.json({ "result": {
+        "New User": newPerson,
+        "New Company": newCompany }})
+        ;
+
+});
+const server = app.listen(8000, () =>
+    console.log(`Server is locked and loaded on port ${server.address().port}!`)
+)
