3e5d864608788ffd23c234b2aac1105712e246d8
raul <raul.alexander.floress@gmail.com>
2022-05-15 14:33:13 -0500

    first commit

--- MERN/Core Assignments/product_manager_part_2/server/routes/products.routes.js
+++
@@ -1,8 +0,0 @@
-const ProductController = require("../controllers/products.controller")
-
-
-module.exports = (app) => {
-    app.post('/api/product', ProductController.addNewProduct);
-    app.get('/api/products', ProductController.getAllProducts);
-    app.get('/api/products/:_id', ProductController.getProductbyID);
-}
